25958f8601c2db33051ff29ed3a8a6646d012363
carrieTseng <echocarriet@gmail.com>
2022-11-11 18:20:46 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <nav class="text-center">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <h2 class="text-center">哈囉 Hello</h2>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+@import '@/assets/stylesheets/all';
+
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
